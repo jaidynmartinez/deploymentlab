d5c745ffa804f0100d6cf0df325cadb6bd629840
Jaidyn Martinez <jaidyn.amarti@gmail.com>
2022-06-30 16:15:14 -0500

    added fake function

--- server/index.js
+++ server/index.js
@@ -1,6 +1,15 @@
 const express = require('express')
 const path = require('path')
 
+var Rollbar = require("rollbar");
+var rollbar = new Rollbar({
+  accessToken: '8377fb9c15484c01be51b1bd642759af',
+  captureUncaught: true,
+  captureUnhandledRejections: true
+});
+
+rollbar.log("Hewwo, wowld!");
+
 const app = express()
 
 app.get('/',function(req,res) {
@@ -10,6 +19,11 @@ app.get('/styles.css',function(req,res) {
     res.sendFile(path.join(__dirname, '../client/styles.css'));
 });
 
+try {
+  nonExistentFunction();
+} catch (error) {
+  console.error(error);
+};
 
 const port = process.env.PORT || 4005
 
